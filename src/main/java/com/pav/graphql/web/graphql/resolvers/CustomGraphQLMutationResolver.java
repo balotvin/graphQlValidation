6339ee5bd2e5f949671accce98692086148d21a6
pavel.balotvin.epam <Pavel_Balotvin@epam.com>
2021-09-15 18:08:18 +0300

    mutation response changes

--- src/main/java/com/pav/graphql/web/graphql/resolvers/CustomGraphQLMutationResolver.java
+++ src/main/java/com/pav/graphql/web/graphql/resolvers/CustomGraphQLMutationResolver.java
@@ -11,7 +11,7 @@ public class CustomGraphQLMutationResolver implements GraphQLMutationResolver {
 
     public CompletableFuture<MutationResponse> testMutation(final String id, final String name, final int value) {
         MutationResponse respModel = new MutationResponse();
-        respModel.setResponse("Return response for Test Mutation:valid data " + id + ", name " + name + ", value " + value);
+        respModel.setResponse("response");
         return CompletableFuture.supplyAsync(() -> respModel);
     }
 }
